5b75c10d3c69cfb32b4bf1c67358fa7f3076624b
Ivan Reshetnikov <resivalex@gmail.com>
2021-12-26 17:31:39 +0300

    Debug

--- src/airflow_weather_recorder/response_storage.py
+++ src/airflow_weather_recorder/response_storage.py
@@ -21,14 +21,16 @@ def save_record(response: str):
         VALUES
     ''', [[datetime.datetime.now().strftime("%Y-%d-%m %H:%M:%S"), response]])
     info = json.loads(response)
-    client.execute('''
-        INSERT INTO weather_log
-        (timestamp, weather_name, temperature, feels_like_temperature, wind_speed)
-        VALUES
-    ''', [
+    record = [
         info['dt'],
         ', '.join([weather['main'] for weather in info['weather']]),
         info['main']['temp'] + CELSIUS_ABSOLUTE_ZERO,
         info['main']['feels_like'] + CELSIUS_ABSOLUTE_ZERO,
         info['wind']['speed']
-    ])
+    ]
+    print(record)
+    client.execute('''
+        INSERT INTO weather_log
+        (timestamp, weather_name, temperature, feels_like_temperature, wind_speed)
+        VALUES
+    ''', record, types_check=True)
